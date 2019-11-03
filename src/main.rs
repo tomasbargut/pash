2ea1e8fb8ed8b13c2bb7e34617859de9be0af027
Tomás Bargut <tomasbarguttabbaj@gmail.com>
2019-11-03 12:42:32 -0300

    Multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::{
-    io::stdin,
+    io::{stdin,stdout, Write},
     process::Command
 };
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
 
-    let command = input.trim();
-    Command::new(command).spawn().unwrap();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+        let mut child = Command::new(command).spawn().unwrap();
+        child.wait();
+    }
 }
